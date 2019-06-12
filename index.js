c41ba6dfbfecdfcbb7964e64c079946060c4622b
jorge silva <j.silva12@ufromail.cl>
2019-06-12 16:37:46 -0400

    flacoloide fix

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-const MiBand = require('miband');
-const bluetooth = require('node-web-bluetooth');
+const MiBand = require("miband");
+const bluetooth = require("webbluetooth").bluetooth;
 
 connectDevice();
 
-async function connectDevice(){
+async function connectDevice() {
+  try {
     const device = await bluetooth.requestDevice({
-        filters: [
-          { services: [ MiBand.advertisementService ] }
-        ],
-        optionalServices: MiBand.optionalServices
-      });
-      
-      const server = await device.gatt.connect();
-      
-      let miband = new MiBand(server);
-      await miband.init();
-      
-      console.log('Notifications demo...')
-      await miband.showNotification('message');
-}
+      filters: [{ services: [MiBand.advertisementService] }],
+      optionalServices: MiBand.optionalServices
+    });
 
+    const server = await device.gatt.connect();
+
+    let miband = new MiBand(server);
+    await miband.init();
+
+    console.log("Notifications demo...");
+    await miband.showNotification("message");
+  } catch (error) {
+    console.log("ERROR: ", error);
+  }
+}
